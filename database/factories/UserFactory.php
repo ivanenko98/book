c452b7dbcd8b739d4c5adb326bab43d888c4c697
westham <ivanenko.oleg.m@gmail.com>
2018-02-19 17:38:41 +0200

    fix bug 1

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,24 @@
+<?php
+use Faker\Generator as Faker;
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+$factory->define(App\User::class, function (Faker $faker) {
+    static $password;
+    return [
+        'name' => $faker->firstName,
+        'surname' => $faker->lastName,
+        'email' => $faker->unique()->safeEmail,
+        'phone' => $faker->unique()->phoneNumber,
+//        'password' => $password ?: $password = bcrypt('secret'),
+        'password' => $password ?: $password = bcrypt('12345678'),
+        'remember_token' => str_random(10),
+    ];
+});
