77cd3c295ce3d6a1215bd4fecd5c00f810bf8e50
westham <ivanenko.oleg.m@gmail.com>
2018-02-28 18:47:23 +0200

    add: book to store

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,10 +1,11 @@
 <?php
 namespace App\Http\Controllers;
-use App\Http\Traits\FormatResponse;
+use App\Traits\FormatResponse;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests, FormatResponse;
--- routes/api.php
+++ routes/api.php
@@ -132,4 +132,6 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::get('/list-genres', 'StoreController@getListGenres');
     /** buy book */
     Route::post('/buy-book', 'StoreController@buyBook');
+    /** book to store */
+    Route::post('/book-to-store', 'StoreController@bookToStore');
 });
