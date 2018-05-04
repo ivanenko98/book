d737ed3de9e82808db013d907bad80143e1fe01f
Maksym <Maksym@MacBook-Air-Maksym.local>
2018-05-04 12:19:20 +0300

    Fixed current_page

--- app/Book.php
+++ app/Book.php
@@ -30,6 +30,7 @@ class Book extends Model
         'rating',
         'genre',
         'translator',
+        'current_page',
     ];
 
     protected $appends = [
@@ -38,6 +39,7 @@ class Book extends Model
         'genre',
         'translator',
         'status',
+        'current_page',
     ];
 
     public function folder(){
@@ -101,6 +103,21 @@ class Book extends Model
 
         return null;
     }
+
+//    Current Page
+    public function getCurrentPageAttribute()
+    {
+        $user = Auth::user();
+
+        $purchased_book = $this->purchases()->where('buyer_id', $user->id)->first();
+
+        if ($purchased_book != null) {
+            return $purchased_book->current_page;
+        }
+
+        return null;
+    }
+//    End
 
     public function getRatingAttribute()
     {
